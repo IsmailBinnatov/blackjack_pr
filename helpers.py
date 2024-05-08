f4b370eb6aff66da90be43309c5240db07b63f1d
Ismll <ismll.acc@mail.ru>
2024-05-08 21:54:52 +0400

    renamed members.py to helpers.py

---
+++ helpers.py
@@ -0,0 +1,13 @@
+def calculate_points(hand):
+    total_points = 0
+
+    for card in hand:
+        value = card.split('.')[1]
+        if value.isdigit():
+            total_points += int(value)
+        elif isinstance(value, str):
+            total_points += 10
+        elif value == 'A':
+            total_points += 11
+
+    return total_points
