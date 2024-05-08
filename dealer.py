37d4dfbbc9141f9a1b6c885c206f51a7e02ee2fd
Ismll <ismll.acc@mail.ru>
2024-05-08 23:42:44 +0400

    deleted unnecessary methods

--- dealer.py
+++ dealer.py
@@ -1,10 +1,4 @@
-from helpers import calculate_points
-
-
 class Dealer:
 
     def __init__(self) -> None:
         self.hand = []
-
-    def __str__(self) -> str:
-        return f'Dealer\'s hand: {self.hand}. \nDealer Points: {calculate_points(self.hand)}'
--- player.py
+++ player.py
@@ -1,11 +1,5 @@
-from helpers import calculate_points
-
-
 class Player:
 
     def __init__(self, name) -> None:
         self.name = name
         self.hand = []
-
-    def __str__(self) -> str:
-        return f'{self.name}\'s hand: {self.hand}. \nYour Points: {calculate_points(self.hand)}'
