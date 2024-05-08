eac8c81fcc230e34e9cd1e6c6ab4bf9e8ed999c7
Ismll <ismll.acc@mail.ru>
2024-05-08 16:11:09 +0400

    deleted class Card, cause moved it to class Deck

--- card.py
+++
@@ -1,7 +0,0 @@
-class Card:
-    
-    def __init__(self) -> None:
-        suits = ['♠', '♥', '♦', '♣']
-        values = [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
-        self.deck = [(f'{suit}.{value}')
-                     for suit in suits for value in values]
