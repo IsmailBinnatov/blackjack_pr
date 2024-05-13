35533c0b9ac89261b7b49378232ec5121ef549af
Ismll <ismll.acc@mail.ru>
2024-05-13 12:36:15 +0400

    created constant file and transfer color codes there, in game.py replaced code to constants

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Colors
+RED = '\033[91m'
+YELLOW = '\033[93m'
+BLUE = '\033[34m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from deck import *
-# from helpers import calculate_points
+from constants import *
 from time import sleep
 
 
@@ -28,66 +28,73 @@ class Game:
         while True:
             if self.calculate_points(self.player.hand) == 21:
                 break
-            sleep(0.5)
+            sleep(0.8)
             action = input('Your turn: (h)it or (s)tand?: ').lower()
 
             if action == 'h':
                 self.player.hand.append(choice(self.deck.deck))
                 print()  # for empty line
-                sleep(0.5)
-                print('You drew a card...')
+                sleep(0.8)
+                print(YELLOW + 'You drew a card...' + RESET)
                 print()  # for empty line
-                sleep(0.5)
-                print(f'Your hand: {self.player.hand}. \nYour Points: {
-                      self.calculate_points(self.player.hand)}')
+                sleep(0.8)
+                print(f'Your hand: {self.player.hand}. \nYour Points: {YELLOW}{
+                      self.calculate_points(self.player.hand)}{RESET}')
                 print()  # for empty line
                 if self.calculate_points(self.player.hand) > 21:
-                    sleep(0.5)
-                    print('Overreach!')
+                    sleep(0.8)
+                    print(RED + 'Overreach!' + RESET)
                     print()  # for empty line
                     break
             elif action == 's':
-                sleep(0.5)
-                print('\nYou stand.\n')
+                sleep(0.8)
+                print(YELLOW + '\nYou stand.\n' + RESET)
                 break
             else:
-                sleep(0.5)
+                sleep(0.8)
                 print('Invalid input! Enter (h) for hit or (s) for stand.')
 
     def dealer_turn(self):
+        self.dealer.hand.append(self.deck.deleted)
+        sleep(0.8)
+        print(YELLOW + 'Dealer shows the second card...' + RESET)
+        print()  # for empty line
+        sleep(0.8)
+        print(f'Dealer\'s hand: {self.dealer.hand}. \nDealer Points: {YELLOW}{
+            self.calculate_points(self.dealer.hand)}{RESET}')
+        print()  # for empty line
         while self.calculate_points(self.dealer.hand) < 17:
             self.dealer.hand.append(choice(self.deck.deck))
-            sleep(0.5)
-            print('Dealer draws a card...')
+            sleep(0.8)
+            print(YELLOW + 'Dealer draws a card...' + RESET)
             print()  # for empty line
-            sleep(0.5)
-            print(f'Dealer\'s hand: {self.dealer.hand}. \nDealer Points: {
-                  self.calculate_points(self.dealer.hand)}')
+            sleep(0.8)
+            print(f'Dealer\'s hand: {self.dealer.hand}. \nDealer Points: {YELLOW}{
+                  self.calculate_points(self.dealer.hand)}{RESET}')
             print()  # for empty line
             if self.calculate_points(self.dealer.hand) > 21:
-                sleep(0.5)
-                print('Dealer overdraw! You win!')
+                sleep(0.8)
+                print('Dealer overdraw!', GREEN + 'You win!' + RESET)
                 print()  # for empty line
                 break
 
     def game_loop(self):
         print()  # for empty line
         sleep(0.5)
-        print('Game starts!')
+        print(YELLOW + 'Game starts!' + RESET)
         print()  # for empty line
-        sleep(0.5)
-        print(f'Player\'s hand: {self.player.hand}\nPlayer Points: {
-              self.calculate_points(self.player.hand)}')
+        sleep(0.8)
+        print(f'Player\'s hand: {self.player.hand}\nPlayer Points: {YELLOW}{self.calculate_points(self.player.hand)}{RESET}')
         print()  # for empty line
         if self.calculate_points(self.player.hand) == 21:
-            print('Blackjack!')
+            print(BLUE + '- Blackjack! -' + RESET)
         print()  # for empty line
-        sleep(0.5)
-        print(f'Dealer\'s hand: {self.dealer.hand}\nDealer Points: {
-              self.calculate_points(self.dealer.hand)}')
+        sleep(0.8)
+        print(f'Dealer\'s hand: {self.dealer.hand + ['?']}\nDealer Points: {YELLOW}{
+              self.calculate_points(self.dealer.hand)}{RESET}')
         print()  # for empty line
         if self.calculate_points(self.dealer.hand) == 21:
-            print('Blackjack!')
+            print(YELLOW + '- Blackjack! -' + RESET)
         print()  # for empty line
 
         self.player_turn()
@@ -97,18 +104,19 @@ class Game:
         player_points = self.calculate_points(self.player.hand)
         dealer_points = self.calculate_points(self.dealer.hand)
         if player_points <= 21 and player_points > dealer_points:
-            sleep(0.5)
-            print('You win!')
+            sleep(0.8)
+            print(GREEN + 'You win!' + RESET)
+            print()  # for empty line
         elif dealer_points <= 21 and (player_points > 21 or dealer_points > player_points):
-            sleep(0.5)
-            print('Dealer wins!')
+            sleep(0.8)
+            print(RED + 'Dealer wins!' + RESET)
             print()  # for empty line
         elif player_points == dealer_points:
-            sleep(0.5)
+            sleep(0.8)
             print('Tie!')
             print()  # for empty line
 
-        sleep(0.5)
+        sleep(0.8)
         print(f'Final hands:\nPlayer hands: {
               self.player.hand}\nDealer hands: {self.dealer.hand}')
         print()  # for empty line
